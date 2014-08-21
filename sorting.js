e3e554df9ef5303310b7822ac4ecde4287ea96df
Vincent Lee <lee.m.vincent@gmail.com>
2014-08-21 01:24:50 -0700

    add js quicksort

--- sorting.js
+++ sorting.js
@@ -1,44 +1,50 @@
-var arrayStart = new Array(11);
-var lowArray = new Array();
-var highArray = new Array();
-var arrayHolder = new Array();
+var arrayStart = new Array(5);
+
 // Randomly create values in an array.
 for(i = 0; i < arrayStart.length; i++) {
   arrayStart [i] = Math.floor(Math.random() * 20) + 1;
 }
 
-function pickPivot(array,left, right) {
-  var pivotIndex = (left + right) / 2;
-  return pivotIndex;
+// Exchanges two values within the array.
+function exchange(array, low, high) {
+  var temp = array[low];
+  array[low] = array[high];
+  array[high] = temp;
 }
 
-function orderArray(array, left, right) {
-  pivotIndex = pickPivot(array, left, right);
-  var pivotValue = array[pivotIndex];
-  var temp = array[right];
-  array[right] = array[pivotIndex];
-  array[pivotIndex] = temp;
-  var storeIndex = left;
-  for(left; left < right - 1; left++) {
-    if (array[i] < pivotValue) {
-      temp = array[i];
-      array[i] = array[left];
-      array[left] = array[i];
+function order(array, low, high) {
+  var pivot = array[Math.floor((low + high) / 2)];
+  while (low <= high) {
+    while(array[low] < pivot) {
+      low++;
+    }
+    while (array[high] > pivot) {
+      high--;
+    }
+    if (low <= high) {
+      exchange(array, low, high);
+      low++;
+      high++;
     }
   }
-  temp = array[right];
-  array[right] = array[storeIndex];
-  array[storeIndex] = temp;
-  return storeIndex;
+  return low;
 }
 
-function quickSort(array, l, h) {
-  var p = quickSort(array, l, h);
-  console.log(l + " " + p + " " + h);
-  if(l < h) {
-
-    quickSort(array, l, p - 1);
-    quickSort(array, p + 1, h);
+function quickSort(array, low, high) {
+  var index;
+  if (array.length > 1) {
+    index = order(array, low, high);
+    if (low < index - 1) {
+      quickSort(array, low, index - 1);
+    }
+    if (index < high) {
+      quickSort(array, index, high);
+    }
   }
-  return 0;
+  return array;
 }
+
+var output = quickSort(arrayStart, 0, arrayStart.length - 1);
+$.each(output, function(i, val) {
+  $("#arrayID").text(val + ",");
+});
