3ad10d31999fc08a94bd6e06dabe95fdf76704c1
Vincent Lee <lee.m.vincent@gmail.com>
2014-08-21 16:08:13 -0700

    add visual

--- sorting.js
+++ sorting.js
@@ -1,10 +1,3 @@
-var arrayStart = new Array(5);
-
-// Randomly create values in an array.
-for(i = 0; i < arrayStart.length; i++) {
-  arrayStart [i] = Math.floor(Math.random() * 20) + 1;
-}
-
 // Exchanges two values within the array.
 function exchange(array, low, high) {
   var temp = array[low];
@@ -29,22 +22,37 @@ function order(array, low, high) {
   }
   return low;
 }
-
 function quickSort(array, low, high) {
   var index;
   if (array.length > 1) {
     index = order(array, low, high);
     if (low < index - 1) {
-      quickSort(array, low, index - 1);
+      setTimeout(quickSort(array, low, index - 1), 5000);
     }
     if (index < high) {
-      quickSort(array, index, high);
+      setTimeout(quickSort(array, index, high), 5000);
     }
+      console.log(arrayStart);
+    $("#arrayID").empty();
+    $.each(array, function(i, num) {
+      $("#arrayID")
+      .append("<div class='red' style='width:" + num*10 + "px;'>" + num + "</div>")
+    });
   }
   return array;
 }
 
+
+var arrayStart = new Array(30);
+
+// Randomly create values in an array.
+for(i = 0; i < arrayStart.length; i++) {
+  arrayStart [i] = Math.floor(Math.random() * 150) + 1;
+}
+console.log(arrayStart);
+$("#arrayID").empty();
+$.each(arrayStart, function(i, num) {
+  $("#arrayID")
+  .append("<div class='red' style='width:" + num + "px;'>" + num + "</div>")
+});
 var output = quickSort(arrayStart, 0, arrayStart.length - 1);
-// $.each(output, function(i, val) {
-//   $("#arrayID").text(val + ",");
-// });
